c7618143c87e007337b3cf06e6ce069eb7bcc318
65160389 <65160389@go.buu.ac.th>
2024-01-17 20:03:40 +0700

    เพิ่มคำอธิบายใน JavaScript

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -23,12 +23,12 @@ document.addEventListener("DOMContentLoaded", function () {
     todos.splice(index, 1);
     renderTodoList();
     }
-    // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo ฟ
+    // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo.
     function toggleComplete(index) {
     todos[index].completed = !todos[index].completed;
     renderTodoList();
     }
-    // แสดงรายการ Todo บนหน้าเว็บ
+    // แสดงรายการ Todo บนหน้าเว็บ.
     function renderTodoList() {
     console.log(todos);
     todoList.innerHTML = "";
